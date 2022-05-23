06ca83c4588a407575b82e78eacb21a940cfd223
unknown <mail4charan807@gmail.com>
2022-05-23 14:17:23 +0530

    commit dance

---
+++ HelloWorld.asm
@@ -0,0 +1,25 @@
+section .text
+  global _main
+  _main:
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, hello
+    mov edx, hello_len
+    int 0x80
+
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, world
+    mov edx, world_len
+    int 0x80
+
+    mov eax, 1
+    int 0x80
+
+
+
+section .data
+  hello: db "Hello"
+  hello_len equ $-hello
+  world: db " World", 0xA
+  world_len equ $-world
