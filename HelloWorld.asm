8a50c03bf7195be642d0af53146f8218b8fef9bf
CharaMan <90620211+CharaMan789@users.noreply.github.com>
2022-05-23 20:41:24 +0530

    commit dance

--- HelloWorld.asm
+++ HelloWorld.asm
@@ -1,24 +1,32 @@
 section .text
   global _main
   _main:
+  
+  ;printing hello
     mov eax, 4
     mov ebx, 1
     mov ecx, hello
     mov edx, hello_len
+   ;kernel call
     int 0x80
     
+   ;printing the underscore character without creating a new line
     mov eax, 4
     mov ebx, 1
     mov ecx, underscore
     mov edx, 1
+    ;kernel call
     int 0x80
 
+   ;printing world
     mov eax, 4
     mov ebx, 1
     mov ecx, world
     mov edx, world_len
+    ;kernel call
     int 0x80
 
+  ;the stop the program after printing, so as to not crash
     mov eax, 1
     int 0x80
 
@@ -28,5 +36,6 @@ section .data
   hello: db "Hello"
   hello_len equ $-hello
   underscore: db "_"
+  ;0xA works as /n in ascii , hence creates a new line
   world: db "World", 0xA
   world_len equ $-world
